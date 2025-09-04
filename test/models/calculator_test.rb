e7230758da9ec8e80aa80230ebd64124791d320c
Raj kumar <raj@madhulata.org>
2025-09-04 03:53:19 +0000

    write 1st test fot empty string

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class CalculatorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "empty string return 0" do
+    assert_equal 0, Calculator.add("")
+  end
 end
