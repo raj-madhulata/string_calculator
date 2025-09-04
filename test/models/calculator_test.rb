1e398afa71e9b7e7a5a6216b125ccf9bef9e6c1a
Raj kumar <raj@madhulata.org>
2025-09-04 04:02:25 +0000

    write test for single value test fail

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -4,4 +4,9 @@ class CalculatorTest < ActiveSupport::TestCase
   test "empty string return 0" do
     assert_equal 0, Calculator.add("")
   end
+
+  test "single number return the value" do
+    assert_equal 1, Calculator.add("1")
+    assert_equal 2, Calculator.add("2")
+  end
 end
