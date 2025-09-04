57982758f607fa51ebfd98e6d98bafaef8f0814c
Raj kumar <raj@madhulata.org>
2025-09-04 04:16:07 +0000

    update add method to pass new line between number

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -2,7 +2,7 @@ class Calculator < ApplicationRecord
   def self.add(string)
     return 0 if string.empty?
 
-    numbers = string.split(",").map(&:to_i)
+    numbers = string.split(/,|\n/).map(&:to_i)
     numbers.sum
   end
 end
