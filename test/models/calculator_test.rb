9a0385c6d0c1577f41b6c330202b0bd986b4c344
Raj kumar <raj@madhulata.org>
2025-09-04 04:08:51 +0000

    test for miltiple numbers

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -14,4 +14,9 @@ class CalculatorTest < ActiveSupport::TestCase
     assert_equal 3, Calculator.add("1,2")
     assert_equal 5, Calculator.add("2,3")
   end
+
+  test "multiple numbers separated by comma return the sum" do
+    assert_equal 6, Calculator.add("1,2,3")
+    assert_equal 10, Calculator.add("1,2,3,4")
+  end
 end
